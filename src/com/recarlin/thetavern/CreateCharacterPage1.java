82444f3d05c37e29b8ac08b775a5a8f800ebe106
recarlin <recarlin@live.com>
2013-07-19 01:13:09 -0400

    Page two is being weird, but I'm getting to the page. The layout isn't working right. I will do more tomorrow.

--- src/com/recarlin/thetavern/CreateCharacterPage1.java
+++ src/com/recarlin/thetavern/CreateCharacterPage1.java
@@ -2,17 +2,30 @@ package com.recarlin.thetavern;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class CreateCharacterPage1 extends Fragment
 {
 	public CreateCharacterPage1() {
 	}
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-	{
-	    return inflater.inflate(R.layout.ccp1, null);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		
+		LinearLayout view = (LinearLayout) inflater.inflate(R.layout.ccp1, null);
+		
+		Button back = (Button) view.findViewById(R.id.page1button);
+    	back.setOnClickListener(new View.OnClickListener() {
+    		@Override
+    		public void onClick(View v) {
+    			Intent page2 = new Intent(getActivity().getApplicationContext(), CreateCharacterPage2.class);
+				startActivityForResult(page2, 0);
+    		}
+    	});
+		return view;
 	}
 }

04932e5a304dd7bb93a055c55d29cfbd94f977db
recarlin <recarlin@live.com>
2013-07-18 23:43:06 -0400

    Working on the UI.

--- gen/com/recarlin/thetavern/R.java
+++ gen/com/recarlin/thetavern/R.java
@@ -24,8 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int fragment_container=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int fragment_container=0x7f080003;
+        public static final int pickClass=0x7f080002;
+        public static final int pickName=0x7f080000;
+        public static final int pickRace=0x7f080001;
     }
     public static final class layout {
         public static final int ccp1=0x7f030000;
